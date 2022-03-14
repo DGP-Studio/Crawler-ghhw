3c1488a85eb31d742efe5269c58c4fd45881f99f
Masterain <i@irain.in>
2022-03-13 18:08:25 -0700

    bug fix

---
+++ tools/Yanfei-TalentQ-Fixer/README.md
@@ -0,0 +1,11 @@
+# 烟绯Q技能数值列表错误修复脚本
+
+本脚本用于修复[https://genshin.honeyhunterworld.com](https://genshin.honeyhunterworld.com)烟绯角色信息中Q技能数值列表错误的问题
+
+## 原始问题截图
+
+![](issue.png)
+
+## 正确列表
+
+![](fix.png)
---
+++ tools/Yanfei-TalentQ-Fixer/yanfei-fixer.py
@@ -0,0 +1,35 @@
+import json
+
+if __name__ == "__main__":
+    print("请将本脚本置于 characters.json 文件同目录下并启动")
+    print("本脚本用于修复烟绯Q技能数据列表错误")
+    input("按回车开始运行...")
+    f = open('characters.json', encoding='utf-8')
+    data = json.load(f)
+    f.close()
+    for character in data:
+        if character['Key'] == "feiyan":
+            stat = character["TalentQ"]["Table"]
+            #print(stat)
+            """
+            内鬼网的原始顺序
+            stat[0]: 技能伤害 --> 不变
+            stat[1]: 丹火印赋予间隔 --> Values 更改为 stat[2]['Values']
+            stat[2]: 重击伤害提升 --> Values 更改为 stat[3]['Values']
+            stat[3]: 持续时间  --> Values 更改为 stat[4]['Values']
+            stat[4]: 冷却时间  --> Values 更改为 stat[5]['Values']
+            stat[5]: 元素能量  --> Values 全部改为 80
+            """
+            character["TalentQ"]["Table"][1]['Values'] = stat[2]['Values'].copy()
+            character["TalentQ"]["Table"][2]['Values'] = stat[3]['Values'].copy()
+            character["TalentQ"]["Table"][3]['Values'] = stat[4]['Values'].copy()
+            character["TalentQ"]["Table"][4]['Values'] = stat[5]['Values'].copy()
+            for key in character["TalentQ"]["Table"][5]['Values']:
+                character["TalentQ"]["Table"][5]['Values'][key] = "80"
+
+    newFileList = json.dumps(data, ensure_ascii=False, indent=4, separators=(',', ':'))
+    f_output = open("characters-YanfeiFixed.json", mode="a", encoding='utf-8')
+    f_output.write(newFileList)
+    f_output.close()
+    input("脚本执行完成")
+
